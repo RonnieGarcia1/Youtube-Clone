6ac4eb4108ee95ebf21895b55fc4a20313c4fedb
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2021-12-03 17:07:09 -0500

    removed youtube-react node module

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import YouTube from 'youtube-react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-     
+     Hello world
       </header>
     </div>
   );
