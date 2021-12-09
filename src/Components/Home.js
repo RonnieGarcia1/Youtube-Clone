8c89e93b8b368ee95a2067ec7223e5be510a7788
Name Ping Louie <namepinglouie@gmail.com>
2021-12-09 15:14:49 -0500

    fixed form with the help of Jerry's group

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,36 +5,35 @@ class Home extends Component{
     constructor() {
         super();
         this.state = {
-            searchStr: ""
+            searchStr: "",
+            data: []
         }
     }
-    
-    // componentDidMount(){
-    //     fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=${this.state.searchStr}&type=video&key=${process.env.REACT_APP_API_KEY}`)
-    //     .then(res => res.json())
-    //     .then(data => console.log(data));
-    // }
 
-    handleSearch = (e) => {
+    handleResult = (e) => {
+        e.preventDefault();
+        fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=${this.state.searchStr}&type=video&key=${process.env.REACT_APP_API_KEY}`)
+        .then(res => res.json())
+        .then((ytData) => {
+            this.setState({
+                data: ytData
+            })
+            console.log(this.state.data);
+        });
+    }
+
+    handleSearch = (e) => {     
         this.setState({
             searchStr: e.target.value,
         })
         console.log(this.state.searchStr);
     }
 
-    handleResult = () => {
-        fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=${this.state.searchStr}&type=video&key=${process.env.REACT_APP_API_KEY}`)
-        .then(res => res.json())
-        .then(data => console.log(data));
-    }
-
-    // need to work on e.target
-
     render(){
         return(
             <div>
                 <form onSubmit={this.handleResult} id="search-bar">
-                    <input type="text" id="text-input" onInput={this.handleSearch} value={this.state.searchStr} />
+                    <input type="text" id="text-input" onChange={this.handleSearch} value={this.state.searchStr} />
                     <button type="submit" id="search-button">Search</button>
                 </form>
                 <div className="video-thumbnails">
