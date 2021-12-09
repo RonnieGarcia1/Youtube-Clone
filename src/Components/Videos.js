98fe97d859c8058c6b9b1d46e404f48d3dc7ea82
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2021-12-09 12:16:22 -0500

    Video card props

--- src/Components/Videos.js
+++ src/Components/Videos.js
@@ -1,8 +1,40 @@
 import { Component } from "react";
+import YouTube from 'react-youtube';
+
 class Videos extends Component{
     render(){
         return(
             <div>
+                <div className="video-card">
+                
+                <div className="videos">
+                <div>
+                <YouTube videoId="AdUw5RdyZxI" opts={{ height: '290', width: '540' }} />
+                <p>EarthCam Live: Times Square in 4k</p>
+                </div>
+                <div>
+                <YouTube videoId="86YLFOog4GM" opts={{ height: '290', width: '540' }} />
+                <p>EarthCam Live: Placeholder</p>
+                </div>
+                <div>
+                <YouTube videoId="BcoIOmZRvEo" opts={{ height: '290', width: '540' }} />   
+                <p>EarthCam Live: Placeholder</p>
+                </div>
+                <div>
+                <YouTube videoId="CUyy8rBnuzY" opts={{ height: '290', width: '540' }} />
+                <p>EarthCam Live: Placeholder</p>
+                </div>
+                <div>
+                <YouTube videoId="fcGDU86DuSo" opts={{ height: '290', width: '540' }} />   
+                <p>EarthCam Live: Placeholder</p>
+                </div>
+                <div>
+                <YouTube videoId="aofxeUrkYQ4" opts={{ height: '290', width: '540' }} />
+                <p>EarthCam Live: Placeholder</p>
+                </div>
+                </div>
+
+                </div>
 
             </div>
         )
