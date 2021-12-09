c9ed9a8bab16d2d2f299861d62b388ef27f33615
Name Ping Louie <namepinglouie@gmail.com>
2021-12-09 12:07:19 -0500

    working on form with little progress

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,19 +2,40 @@ import { Component } from 'react';
 import YouTube from 'react-youtube';
 
 class Home extends Component{
+    constructor() {
+        super();
+        this.state = {
+            searchStr: ""
+        }
+    }
     
-    componentDidMount(){
-        fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=dog&type=video&key=${process.env.REACT_APP_API_KEY}`)
+    // componentDidMount(){
+    //     fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=${this.state.searchStr}&type=video&key=${process.env.REACT_APP_API_KEY}`)
+    //     .then(res => res.json())
+    //     .then(data => console.log(data));
+    // }
+
+    handleSearch = (e) => {
+        this.setState({
+            searchStr: e.target.value,
+        })
+        console.log(this.state.searchStr);
+    }
+
+    handleResult = () => {
+        fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=${this.state.searchStr}&type=video&key=${process.env.REACT_APP_API_KEY}`)
         .then(res => res.json())
         .then(data => console.log(data));
     }
 
+    // need to work on e.target
+
     render(){
         return(
             <div>
-                <form id="search-bar">
-                    <input id="text-input" />
-                    <button id="search-button">Search</button>
+                <form onSubmit={this.handleResult} id="search-bar">
+                    <input type="text" id="text-input" onInput={this.handleSearch} value={this.state.searchStr} />
+                    <button type="submit" id="search-button">Search</button>
                 </form>
                 <div className="video-thumbnails">
                     Video Thumbnail
